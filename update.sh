72528499581f18460270f493df3a5129042f5080
Michael Power <powerm3@tcd.ie>
2019-01-15 15:01:03 +0000

    update linux script

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+sudo apt-get update        # Fetches the list of available updates
+sudo apt-get upgrade       # Strictly upgrades the current packages
+sudo apt-get dist-upgrade  # Installs updates (new ones)
+sudo apt autoremove
